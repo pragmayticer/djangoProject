135ba8afbe3e13222acca13ed3e34f737c84d842
ITsecurity <itsecurity@hsalbsig.de>
2021-06-04 12:18:41 +0200

    updated su change user

--- itsec/itsecapp/views.py
+++ itsec/itsecapp/views.py
@@ -11,7 +11,13 @@ import MySQLdb
 from datetime import datetime
 import time
 
+import subprocess
+import pwd
+import os
+
 def home_view(request):
+    sudo_password = 'HS-albsig-ITS2021'
+
     reader = SimpleMFRC522()
     rfid_id: int = 0
     rfid_id, text = reader.read()
@@ -20,13 +26,17 @@ def home_view(request):
     current_usermail = request.user.email
     current_user = User.objects.get(email=current_usermail)
     all_RFID_Daten: QuerySet=RFID_Daten.objects.filter(user=current_user)
-    print(all_RFID_Daten)
     if current_user.is_superuser:
         current_RFID = int(all_RFID_Daten[0].RFID_Id)
         if current_RFID == rfid_id:
             all_RFID_Daten: QuerySet=RFID_Daten.objects.all()
             b = RFID_Daten(RFID_Id=current_RFID, name=current_user.username, login=datetime.now(), user=current_user)
             b.save()
+            command = 'su - webadmin'
+            command = command.split()
+
+            cmd1 = subprocess.Popen(['echo', sudo_password], stdout=subprocess.PIPE)
+            cmd2 = subprocess.Popen(['sudo'] + command, stdin=cmd1.stdout, stdout=subprocess.PIPE)
             return render(request, "./base.html", {'posts': all_RFID_Daten})
     else:
         current_RFID = int(all_RFID_Daten[0].RFID_Id)
@@ -35,9 +45,14 @@ def home_view(request):
         if rfid_id == current_RFID:
             b = RFID_Daten(RFID_Id=current_RFID, name=current_user.username, login=datetime.now(), user=current_user)
             b.save()
+            command = 'su - {user}'.format(user=current_user.username.lower())
+            command = command.split()
+
+            cmd1 = subprocess.Popen(['echo', sudo_password], stdout=subprocess.PIPE)
+            cmd2 = subprocess.Popen(['sudo'] + command, stdin=cmd1.stdout, stdout=subprocess.PIPE)
             return render(request, "./base.html", {'posts': all_RFID_Daten})
         else:
-            print("nicht gleich")
+            return HttpResponse("Kein Zugriff möglich")
     return HttpResponse("Kein Zugriff möglich")
 
 def login_view(request):
